82879e4f0631bbe2e5e92b1c8c3e9b11fbe470c2
shohan12727 <aishohan001@gmail.com>
2025-02-13 13:45:20 +0600

    again

---
+++ return.js
@@ -0,0 +1,14 @@
+function tentimes (number){
+    const result = number * 10 ;
+    return result ;
+}
+
+function cutHalf (number){
+    const half = number / 2;
+    return half;
+}
+
+// tentimes (7);
+
+const abc = tentimes(22);
+console.log (abc);
