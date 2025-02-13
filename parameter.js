5dcb02f8b303251d88e4ad6790b8fd0fe7a46ac1
shohan12727 <aishohan001@gmail.com>
2025-02-13 13:25:35 +0600

    function

---
+++ parameter.js
@@ -0,0 +1,28 @@
+// function vaatkhau (){
+//     console.log ("hand wash koro");
+//     console.log ("boso");
+//     console.log ("khau");
+// } 
+// vaatkhau   ();
+
+function square (number){
+    
+    console.log("value of the parameter",number);
+    const borgo = number * number ;
+    console.log ("square of the given number is:",borgo);
+}
+square(4);
+// console.log ("--------");
+square(12);
+square(10);
+
+
+console.log("---------");
+
+function add (num1,num2){
+    const sum = num1+num2;
+    console.log(sum);
+}
+add (12,56);
+add(12,98);
+
---
+++ sh_nsu.js
@@ -0,0 +1,7 @@
+function doubleIt (number){
+    const double = number*2;
+    console.log (number,double);
+
+}
+console.log ("I will call the function");
+doubleIt (55);
